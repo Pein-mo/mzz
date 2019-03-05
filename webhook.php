644b3d2b5047ea561a7ba0863a4489922e179356
mozhenzhu <251439200@qq.com>
2019-03-05 22:45:08 +0800

    重新提交hook

--- webhook.php
+++ webhook.php
@@ -1,14 +1,49 @@
-<?php 
-error_reporting(1);
-$token = "mozhenzhu";
-$path="/www/wwwroot/git.mo1120.com";
+<?php
 
-$json = json_decode(file_get_contents('php://input'), true);
- 
-if (empty($json['token']) || $json['token'] !== $token) {
-    exit('error request');
-}
 
-echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+    $target = '/www/wwwroot/git.mo1120.com'; // 生产环境web目录
+    //密钥
+    $secret = "mozhenzhu";
+    $wwwUser = 'www';
+    $wwwGroup = 'www';
 
-?>
+    //日志文件地址
+    $fs = fopen('./gitHubAuto_hook.log', 'a');
+
+    //获取GitHub发送的内容 
+    $json = file_get_contents('php://input');
+    $content = json_decode($json, true);
+    //github发送过来的签名  
+    $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
+    if (!$signature) {
+       fclose($fs);
+       return http_response_code(404);
+     }
+
+    list($algo, $hash) = explode('=', $signature, 2);
+    //计算签名  
+    $payloadHash = hash_hmac($algo, $json, $secret);
+
+    // 判断签名是否匹配  
+    if ($hash === $payloadHash) {
+        $cmd = "cd $target && git pull";
+        $res = shell_exec($cmd);
+
+        $res_log .= 'Success:'.PHP_EOL;
+        $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL;
+        $res_log .= $res.PHP_EOL;
+        $res_log .= '======================================================================='.PHP_EOL;
+
+        fwrite($fs, $res_log);
+        $fs and fclose($fs);
+
+
+      } else {
+            $res_log  = 'Error:'.PHP_EOL;
+            $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '>个commit：' . PHP_EOL;
+            $res_log .= '密钥不正确不能pull'.PHP_EOL;
+            $res_log .= '======================================================================='.PHP_EOL;
+           fwrite($fs, $res_log);
+           $fs and fclose($fs);
+      }
