d36a106955aa2bf4686144534dede61ecdbe2707
mozhenzhu <251439200@qq.com>
2019-03-05 21:45:25 +0800

    webhook 提交

---
+++ webhook.php
@@ -0,0 +1,15 @@
+<?php 
+
+$secret = "mozhenzhu";
+
+$path="/www/wwwroot/git.mo1120.com";
+$signature = $_SERVER['HTTP_X_HUB_SIGNATUR'];
+if($signature){
+	$hash = "sha1=".hash_hmac('sha1',file_get_contents("php://input"),$secret);
+	if(strcmp($signature,$hash) == 0){
+		echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+		exit;
+	}
+}
+http_response_code(404);
+?>
